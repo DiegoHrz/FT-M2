996a0912149e50e2719d19eec58a82f8acc1d854
DiegoHrz <hernandezwdiego@gmail.com>
2023-08-26 23:53:18 -0500

    Add Homework 8 part 1

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/App.js
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Zoo from "./components/Zoo/Zoo.jsx";
+import Animals from "./components/Animals/Animals.jsx";
 
 export default function App() {
   return (
     <div>
       <Zoo />
+      <Animals />
     </div>
   );
 }
--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
@@ -3,10 +3,23 @@ import React from 'react';
 
 export default class Animals extends React.Component {
   constructor(props) {
-    super();
+    super(props);
   }
 
   render() {
-    return <div></div>
-  }
+    console.log(this.props.animals)
+    return (
+      <div>
+        {this.props.animals.map((animal, index)=>{
+          return (
+            // todos los elementos de una iteracion requieren una key
+            <div key={index}> 
+              <h5>{animal.name}</h5>
+              <img src={animal.image} alt={animal.name} width='300px' />
+              <span>{animal.specie}</span>
+            </div>
+          )
+        })}
+      </div>
+  )}
 }
