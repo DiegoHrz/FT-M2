1d7fc5e1277f3c0c7ca7e449ee29707f96307024
DiegoHrz <hernandezwdiego@gmail.com>
2023-09-02 23:58:54 -0500

    commit

--- 06-React-Intro/homework/01 - Exercises/src/components/Botones.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Botones.jsx
@@ -10,6 +10,7 @@ class Botones extends React.Component{
                 <button onClick={() => alert(this.props.alerts.m1)} >Módulo 1</button>
                 <button onClick={() => alert(this.props.alerts.m2)} >Módulo 2</button>
             </div>
+            
         )
     }   
 }
